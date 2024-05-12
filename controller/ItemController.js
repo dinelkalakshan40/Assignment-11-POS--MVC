b0a3a7dc0b7c5a35d1136e1e06ed1e560ceb0c9f
dinelka <newlakshan3@gmail.com>
2024-05-12 22:49:28 +0530

    update item validation

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -36,6 +36,23 @@ $('#item-add-btn').on('click',()=> {
     var itemPrice =$('#item-price').val();
     var itemQty =$('#item-qty').val();
 
+    if (!/^C-00\d*$/.test(itemCode)) {
+        alert("Item code Invalid");
+        return;
+    }
+    if (itemName.length < 4){
+        alert("Item Name Invalid");
+        return;
+    }
+    if (!/^\d{1,5}\.00$/.test(itemPrice)){
+        alert("Item Price Invalid");
+        return;
+    }
+    if (!/^\d{1,4}$/.test(itemQty)){
+        alert("Item Qty Invalid");
+        return;
+    }
+
     let itemModel = new ItemModel(itemCode,itemName,itemPrice,itemQty);
 
     let item = {
@@ -54,4 +71,47 @@ $('#item-add-btn').on('click',()=> {
     $('#item-qty').val('');
 
     alert("item Saved");
+});
+$("#item-code").focus(() => {
+    const itemCode = $('#item-code').val();
+
+    // Validate customer ID using CID-001 pattern
+    if (!/^C-00\d*$/.test(itemCode)) {
+        $('#itemCodeAlert').html("Please enter a valid Code in the format  C-00").css("color", "red"); // Show alert in red color under the input field
+    } else {
+        $('#itemCodeAlert').html("").css("color", ""); // Clear the alert message if the ID format is valid
+    }
+});
+
+$("#item-name").focus(() => {
+    const itemName = $('#item-name').val();
+
+
+    if (itemName.length < 4) {
+        $('#itemNameAlert').html("Item-Name should have at least 4 characters").css("color", "red");
+    } else {
+        $('#itemNameAlert').html("").css("color", "");
+    }
+});
+
+$("#item-price").focus(() => {
+    const itemPrice = $('#item-price').val();
+
+
+    if (!/^\d{1,5}\.00$/.test(itemPrice)) {
+        $('#itemPriceAlert').html("item Price end .00").css("color", "red");
+    } else {
+        $('#itemNameAlert').html("").css("color", "");
+    }
+});
+
+$("#item-qty").focus(() => {
+    const itemQty = $('#item-qty').val();
+
+
+    if (!/^\d{1,4}$/.test(itemQty)) {
+        $('#itemQtyAlert').html("item Qty should between 1-4 ").css("color", "red");
+    } else {
+        $('#itemQtyAlert').html("").css("color", "");
+    }
 });
